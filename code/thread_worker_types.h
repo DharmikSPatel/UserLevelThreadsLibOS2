7f3605414119ca404c62b92d3ba0323d81cc2625
Dharmik S. Patel <dsp187@popsicle.cs.rutgers.edu>
2024-02-22 13:20:02 -0500

    small code done

--- code/thread_worker_types.h
+++ code/thread_worker_types.h
@@ -6,15 +6,14 @@
 typedef unsigned int worker_t;
 
 // Define the states a worker thread can be in
-#define READY 0
-#define SCHEDULED 1
+#define RUNNING 0
+#define READY 1
 #define BLOCKED 2
-#define RUNNING 3
 
 typedef struct TCB
 {
     worker_t thread_id;         // Unique ID to identify each worker thread //correct
-    int thread_status;          // Status of the thread (READY, SCHEDULED, BLOCKED, etc.) //correct and implemented
+    int thread_status;          // Status of the thread (RUNNING, READY, BLOCKED) //correct and implemented
     ucontext_t thread_context;  // Context needed for running the thread on a CPU
     void* thread_stack;         // Stack for the thread
     int thread_priority;        // Priority of the thread (for MLFQ)
